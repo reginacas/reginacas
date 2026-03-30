bc7aff2773706b20c85a343d32e6673e3356c535
reginacas <reginacas97@gmail.com>
2026-03-30 19:35:38 +0200

    Prepare root GitHub Pages URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://reginacas.github.io",
-  base: "/reginacas",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
